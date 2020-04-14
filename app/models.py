cc9bc4a8a277876906a4771839a5fb26de2e8ee2
Bing <odowd.bing@gmail.com>
2020-04-14 18:58:44 +0200

    added tablename for postgres

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from flask_login import UserMixin
 from app import db, login_manager
 
 class Posts(db.Model):
+    __tablename__ = 'posts'
+
     id = db.Column(db.Integer, primary_key=True)
     poster_email = db.Column(db.String(120), unique=False)
     date_posted = db.Column(db.Date, index=True, unique=False)

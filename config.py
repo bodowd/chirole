32dadd4619dd7dbb9abdee0b6a000a29e8c93516
Bing <odowd.bing@gmail.com>
2020-04-19 15:22:55 +0200

    changed to a json config to try fix supervisor

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
+import json
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ['SECRET_KEY']
-    APP_USERNAME = os.environ['APP_USERNAME']
-    APP_PASSWORD = os.environ['APP_PASSWORD']
+    SECRET_KEY = config.get('SECRET_KEY')
+    APP_USERNAME = config.get('APP_USERNAME')
+    APP_PASSWORD = config.get('APP_PASSWORD')
     CHARGE_AMOUNT_USD = 115
+    STRIPE_PUBLISHABLE_KEY = config.get('STRIPE_PUBLISHABLE_KEY')
+    STRIPE_SECRET_KEY = config.get('STRIPE_SECRET_KEY')
